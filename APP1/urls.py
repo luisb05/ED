546c70fbadb61c42aca70d24b20d4935c8829ff4
luisb05 <44504830+luisb05@users.noreply.github.com>
2024-07-24 20:52:05 -0600

    Cambios en Menu

--- APP1/urls.py
+++ APP1/urls.py
@@ -1,6 +1,16 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('index/', views.index, name='index'),
+# ]
+
+from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views  # Assuming you have views in the current directory
 
 urlpatterns = [
+    path('', views.index, name='index'),  # Add this line
     path('index/', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
