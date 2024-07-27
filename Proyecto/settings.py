9ebcea1336366059f1b451b9ba8fdbbb15f0dac6
luisb05 <44504830+luisb05@users.noreply.github.com>
2024-07-26 21:06:27 -0600

    Cambios de Vistas 4

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# Ruta al directorio de archivos estáticos
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
